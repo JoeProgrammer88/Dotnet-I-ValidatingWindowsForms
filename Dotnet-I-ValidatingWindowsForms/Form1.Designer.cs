35b6dce8faf5d17e3e7c8b9b42501fa81046a612
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2021-05-18 11:36:02 -0700

    Working errorProvider example functional

--- Dotnet-I-ValidatingWindowsForms/Form1.Designer.cs
+++ Dotnet-I-ValidatingWindowsForms/Form1.Designer.cs
@@ -30,12 +30,91 @@ namespace Dotnet_I_ValidatingWindowsForms
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.errProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Age:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(44, 168);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(134, 67);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit Data";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(107, 34);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 23);
+            this.txtAge.TabIndex = 2;
+            this.txtAge.Validating += new System.ComponentModel.CancelEventHandler(this.TxtAge_Validating);
+            // 
+            // errProvider
+            // 
+            this.errProvider.BlinkRate = 400;
+            this.errProvider.ContainerControl = this;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(107, 85);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 23);
+            this.txtName.TabIndex = 4;
+            this.txtName.Validating += new System.ComponentModel.CancelEventHandler(this.TxtName_Validating);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(401, 289);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Error Provider Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.ErrorProvider errProvider;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Dotnet-I-ValidatingWindowsForms/Form1.cs
+++ Dotnet-I-ValidatingWindowsForms/Form1.cs
@@ -17,5 +17,71 @@ namespace Dotnet_I_ValidatingWindowsForms
             InitializeComponent();
         }
 
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren())
+            {
+                MessageBox.Show("All data was valid");
+            }
+            else
+            {
+                MessageBox.Show("There is some invalid data");
+            }
+        }
+
+        private void TxtAge_Validating(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                Convert.ToByte(txtAge.Text);
+                errProvider.SetError(txtAge, string.Empty);
+            }
+            catch (FormatException)
+            {
+                errProvider.SetError(txtAge, "Age must be a number. Ex. 30");
+                e.Cancel = true; // Setting the Cancel property to true, marks control as invalid
+            }
+            catch (OverflowException)
+            {
+                errProvider.SetError(txtAge, "Age must be between 0 - 255");
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // By default AutoValidate will prevent invalid controls from
+            // losing focus. This can be disabled completely or changed to
+            // allow focus to move to another control, as shown here.
+            AutoValidate = AutoValidate.EnableAllowFocusChange;
+        }
+
+        private void TxtName_Validating(object sender, CancelEventArgs e)
+        {
+            if (!IsPresent(txtName))
+            {
+                e.Cancel = true;
+                errProvider.SetError(txtName, "Name is required");
+            }
+            else
+            {
+                errProvider.SetError(txtName, string.Empty);
+            }
+        }
+
+        /// <summary>
+        /// Returns true if textbox text is not a null
+        /// or whitespace
+        /// </summary>
+        /// <param name="box">The textbox to validate</param>
+        private bool IsPresent(TextBox box)
+        {
+            if (!string.IsNullOrWhiteSpace(box.Text))
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
